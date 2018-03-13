b00f87547a756d6198484a2ecb86810156ac0c68
Vinesy <daveydreamer@gmail.com>
2018-03-13 19:31:13 -0400

    Added tweaks based on Lab Testing.

--- script.groovy
+++ script.groovy
@@ -23,11 +23,20 @@ private def executeOnShell(String unit, String command) {
 private def executeOnShell(String unit, String command, File workingDir) {
   println "Starting " + unit
   def startTime = new Date()
+  
+  def file = new File(unit + ".error." + scriptStartTime.format("yyyyMMdd_HHmmss") + ".log")
+  if(file.exists()){file.delete()}
+  file << unit + "\n"
+  file << command + "\n"	
+  
   def process = new ProcessBuilder(addShellPrefix(command))
                                     .directory(workingDir)
                                     .redirectErrorStream(true) 
                                     .start()
+  //need to write the input stream live.  Seems to choke otherwise on processes with a lot of stdout
+  process.inputStream.eachLine {file << it << "\n"}
   process.waitFor();
+	
   //See How long the command took
   def endTime = new Date()
   TimeDuration duration = TimeCategory.minus(endTime, startTime)
@@ -35,15 +44,14 @@ private def executeOnShell(String unit, String command, File workingDir) {
   //If the process failed, Save the processes output.
   if(process.exitValue() != 0)
   {
-    File file = new File(unit + ".error." + scriptStartTime.format("yyyyMMdd_HHmmss") + ".log")
-    if(file.exists()){file.delete()}
-    file << unit + "\n"
-    file << command + "\n"
-    file << "Return Code: " + process.exitValue() + "\n"
-    process.inputStream.eachLine {file << it}
+    file << "Return Code: " + process.exitValue() + "\n"    
     file << "\n"
     println "See " + file.getAbsolutePath() + " for more details."
+  }else{
+    //Delete the file, no need to save it process executed successfully
+    file.deleteOnExit();
   }
+	
   return process.exitValue();
 
 }
@@ -70,7 +78,6 @@ cmds.put("cmd4", "~/scripts/sleep5.sh")
 cmds.put("cmd5", "~/scripts/sleep3.sh")
 cmds.put("cmd6", "~/scripts/error.sh")
 
-
 println "Starting Execution of Tasks"
 
 //Global Start Time
@@ -80,18 +87,13 @@ println scriptStartTime.format("yyyyMMdd-HH:mm:ss.SSS")
 
 def returnCode = 0
 
-//exec init
-
-
 try {
-  //Exec Libs
+
   //Take the list of commands and and send them to the thread pool to be processed.
   List<Future> futures = cmds.collect{entry->
     threadPool.submit({->
     executeOnShell(entry.key, entry.value) } as Callable);
   }
-
-  //Exec Exes
 
   // recommended to use following statement to ensure the execution of all tasks.
   //  Holds until each future is available.
